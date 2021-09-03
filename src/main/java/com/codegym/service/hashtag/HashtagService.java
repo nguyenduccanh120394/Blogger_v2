1eeae48033caf5f1228ff829f380de434b15d4cb
ADMIN <duccanhh94@gmail.com>
2021-09-03 17:50:03 +0700

    CRUD Hashtag

---
+++ src/main/java/com/codegym/service/hashtag/HashtagService.java
@@ -0,0 +1,31 @@
+package com.codegym.service.hashtag;
+import com.codegym.model.Hashtag;
+import com.codegym.service.hashtag.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.codegym.repository.*;
+import java.util.Optional;
+@Service
+public class HashtagService implements IHashtagService{
+    @Autowired
+    private IHashtagRepository hashtagRepository;
+    @Override
+    public Iterable<Hashtag> findAll() {
+        return hashtagRepository.findAll();
+    }
+
+    @Override
+    public Optional<Hashtag> findById(Long id) {
+        return hashtagRepository.findById(id);
+    }
+
+    @Override
+    public Hashtag save(Hashtag hashtag) {
+        return hashtagRepository.save(hashtag);
+    }
+
+    @Override
+    public void remove(Long id) {
+        hashtagRepository.deleteById(id);
+    }
+}
---
+++ src/main/java/com/codegym/service/hashtag/IHashtagService.java
@@ -0,0 +1,5 @@
+package com.codegym.service.hashtag;
+import com.codegym.model.*;
+import com.codegym.service.*;
+public interface IHashtagService extends IGeneralService<Hashtag>{
+}
