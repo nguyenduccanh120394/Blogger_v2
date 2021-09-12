264dde320d6232c91ad093be5a64853ab4dedf0a
ADMIN <duccanhh94@gmail.com>
2021-09-12 07:35:26 +0700

    pull Qa

--- src/main/java/com/codegym/message/request/CommentPostCreate.java
+++ src/main/java/com/codegym/message/request/CommentPostCreate.java
@@ -7,4 +7,28 @@ import lombok.Data;
 public class CommentPostCreate {
     private String text;
     private String id;
+
+    public CommentPostCreate(String text, String id) {
+        this.text = text;
+        this.id = id;
+    }
+
+    public CommentPostCreate() {
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
 }
